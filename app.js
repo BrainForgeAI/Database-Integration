f0c5407fc3df82a46ff76731e111135d50904003
varrahan <varrahan@gmail.com>
2024-09-05 23:03:32 -0400

    Added app.js to run API

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import userRouter from './src/routers/userRouter.js';
+import gameRouter from './src/routers/gameRouter.js';
+
+const app = express();
+const port = process.env.API_PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API root!');
+});
+
+app.use(express.json());
+app.use('/aspectus/users', userRouter);
+app.use('/aspectus/game', gameRouter);
+ 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}. It is available on http://localhost:${port}`);
+});
