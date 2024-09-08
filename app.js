b3bab913d2618914b4cd67ce78fb8f964a7b53c1
varrahan <varrahan@gmail.com>
2024-09-07 21:16:21 -0400

    Reformated to include host port for local development

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+import cors from 'cors'
 import express from 'express';
 import userRouter from './src/routers/userRouter.js';
 import gameRouter from './src/routers/gameRouter.js';
 
+// Initialize application and ports
 const app = express();
-const port = process.env.API_PORT;
+const port = process.env.CONTAINER_PORT;
+const host = process.env.HOST_PORT;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the API root!');
-});
-
+app.use(cors())
 app.use(express.json());
 app.use('/aspectus/users', userRouter);
 app.use('/aspectus/game', gameRouter);
  
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}. It is available on http://localhost:${port}`);
+    console.log(`Server is running on port ${port} inside the docker container`);
+    console.log(`Application can be accessed through web with http://localhost:${host}/aspectus/`)
 });
